0c1028bd52ba0a77c1008519169f5491d6ca3cd0
erlams <e@gmail.com>
2023-12-02 12:41:22 +0400

    Project 2

--- script.js
+++ script.js
@@ -8,14 +8,30 @@ const personalMovieDB = {
     privat: false
 }
 
-let answer1 = prompt('Axirinci baxdiginiz film');
-let answer2 = prompt('Nech bal ile qiymetlendirirsiniz?');
+for (let i = 0; i < numberOfFilms; i++) {
+    let answer1 = prompt('Axirinci baxdiginiz film');
+    let answer2 = prompt('Nech bal ile qiymetlendirirsiniz?');
 
+    if (answer1 != '' && answer1 != null && answer2 != '' && answer2 != null) {
+        personalMovieDB.movies[answer1] = answer2;
+    } else {
+        console.log('Sehv bas verdi');
+        i--;
+    }
+}
 
-personalMovieDB.movies[answer1] = answer2;
+if (personalMovieDB.count < 10) {
+    console.log('Az fiilme baxmisiniz');
+} else if (personalMovieDB.count < 30) {
+    console.log('Klassik film izleyicisi');
+} else if (personalMovieDB > 30) {
+    console.log('Siz kinomensiniz');
+} else {
+    console.log('Sehv bas verdi');
+}
 
 console.log(personalMovieDB);
 
-let netice = ` Siz ${numberOfFilms} kinoya baxmisiniz. Sizin axirinci baxdiginiz ${answer1} kinosudur. Siz bu kinonu ${answer2} bal ile qiymetlendirdiniz`;
+let netice = ` Siz ${numberOfFilms} kinoya baxmisiniz. Sizin axirinci baxdiginiz  kinosudur. Siz bu kinonu  bal ile qiymetlendirdiniz`;
 
 document.getElementById('kino').innerHTML = netice;
